0c2537b826d4dd47c88ecf0d629a8537fbc17dd3
°• Crying •° <82006556+Crying-0001@users.noreply.github.com>
2021-08-22 16:46:51 -0500

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -1,6 +1,6 @@
 module.exports = ({
 name: "encendido",
 aliases: "update",
-code: `$title[<a:Shiinacheck:829320971432230922> $updateCommands *Comandos actualizados*]$color[GREEN]
+code: `$title[🎁 $updateCommands *Comandos actualizados*]$color[GREEN]
 $onlyForIDs[603313659590082638;817148330865197158;818964129284751371;{title:<a:Shiinadenny:829320362767548473> Tu necesitas ser unos de los administradores del bot}{color:RED}]`
 })
